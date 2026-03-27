77057583e6c8d80056b8f4ab880afd70426049ea
Vivek DK <156738949+Vivek-DK@users.noreply.github.com>
2026-03-27 17:13:14 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+const { protect } = require("../middleware/authMiddleware");
 const { loginUser } = require("../controllers/authController");
 
 // POST /api/auth/login
-router.post("/login", loginUser);
+router.post("/login", protect, loginUser);
 
 module.exports = router;
